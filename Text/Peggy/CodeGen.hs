245ecd3bd3af0c73ef92843636a20b5a65751784
Hideyuki Tanaka <tanaka.hideyuki@gmail.com>
2011-09-12 13:23:51 -0700

    refactor codegen

--- Text/Peggy/CodeGen.hs
+++ Text/Peggy/CodeGen.hs
@@ -1,10 +1,12 @@
 {-# Language TemplateHaskell #-}
+{-# Language TupleSections #-}
 
 module Text.Peggy.CodeGen (
   genCode,
   removeLeftRecursion,
   ) where
 
+import Control.Applicative
 import Language.Haskell.Meta
 import Language.Haskell.TH
 import Text.Peggy
@@ -13,48 +15,44 @@ import Text.Peggy.SrcLoc
 import Text.Peggy.Normalize
 import Text.Peggy.LeftRec
 
-genCode :: Syntax -> [Dec]
+genCode :: Syntax -> Q [Dec]
 genCode = generate . removeLeftRecursion . normalize
 
-generate :: Syntax -> [Dec]
-generate defs = defDerivs : instDerivs : parse : parsers where
+generate :: Syntax -> Q [Dec]
+generate defs = do
+  sequence $ [defDerivs, instDerivs, parse] ++ parsers
+  where
   derivsName = mkName "UserDerivs"
-  
-  defDerivs = DataD [] derivsName [] [con] [] where
-    con = RecC derivsName $ udvChar : udvPos : map toDV defs
-    
-    udvChar = (mkName "udv_char", NotStrict,
-               AppT (AppT (ConT ''Result) (ConT derivsName)) (ConT ''Char))
-    udvPos  = (mkName "udv_pos",  NotStrict, ConT ''SrcPos)
-    
-    toDV (Definition nont typ _) =
-      ( mkName ("udv_" ++ nont)
-      , NotStrict
-      , derivType typ
-      )
-  
+
+  defDerivs = dataD (cxt []) derivsName [] [con] [] where
+    con = recC derivsName $ udvChar : udvPos : map toDV defs
+    udvChar = (mkName "udv_char", NotStrict, ) <$>
+              (conT ''Result `appT` conT derivsName `appT` conT ''Char)
+    udvPos  = (mkName "udv_pos", NotStrict, ) <$> conT ''SrcPos
+
+  toDV (Definition nont typ _) =
+    (, NotStrict, derivType typ) <$> return (mkName ("udv_" ++ nont))
+
   instDerivs =
-    InstanceD [] (AppT (ConT ''Derivs) $ ConT derivsName)
-    [ ValD (VarP 'dvPos) (NormalB $ VarE $ mkName "udv_pos") []
-    , ValD (VarP 'dvChar) (NormalB $ VarE $ mkName "udv_char") []
+    instanceD (cxt []) (conT ''Derivs `appT` conT derivsName)
+    [ valD (varP 'dvPos) (normalB $ varE $ mkName "udv_pos") []
+    , valD (varP 'dvChar) (normalB $ varE $ mkName "udv_char") []
     ]
-  
-  parse = 
-    FunD fname
-    [Clause [VarP pos, VarP str]
-     (NormalB $ VarE d)
-     [ ValD (VarP d)
-       (NormalB $ foldl1 AppE $ [ConE derivsName, VarE pchar, VarE pos] ++ pp)
+
+  parse =
+    funD fname
+    [clause [varP pos, varP str]
+     (normalB $ varE d)
+     [ valD (varP d)
+       (normalB $ appsE $ [conE derivsName, varE pchar, varE pos] ++ pp defs)
        []
-     , ValD (VarP pchar)
-       (NormalB $ CaseE (VarE str)
-        [ Match
-          (InfixP (VarP c) '(:) (VarP cs))
-          (NormalB $ app2c 'Parsed (app2 fname (app2 'advance (VarE pos) (VarE c)) (VarE cs)) (VarE c))
+     , valD (varP pchar)
+       (normalB $ caseE (varE str)
+        [ match (infixP (varP c) '(:) (varP cs))
+          (normalB [| Parsed ($(varE fname) ($(varE pos) `advance` $(varE c)) $(varE cs)) $(varE c) |])
           []
-        , Match
-          WildP
-          (NormalB $ app1c 'Failed (app2c 'ParseError (app1c 'LocPos $ app1 'dvPos (VarE d)) (LitE (StringL ""))))
+        , match wildP
+          (normalB [| Failed $ ParseError (LocPos $ dvPos $(varE d)) "" |])
           []
         ])
        []
@@ -69,74 +67,65 @@ generate defs = defDerivs : instDerivs : parse : parsers where
       pos = mkName "pos"
       str = mkName "str"
 
-      pp = map
-        (\(Definition nont _ _) ->
-          app2 'unParser (VarE $ mkName nont) (VarE d))
-        defs
+      pp = map (\(Definition nont _ _) -> [| unParser $(varE $ mkName $ nont) $(varE d) |])
   
   parsers = concatMap gen defs
   
   gen (Definition nont typ e) =
-    [ SigD (mkName nont) $ parserType typ
-    , FunD (mkName nont)
-     [Clause [] (NormalB $ genP e) []]]
+    [ sigD (mkName nont) $ return $ parserType typ
+    , funD (mkName nont)
+      [clause [] (normalB $ genP e) []]]
   
   genP e = case e of
     Terminals str ->
-      app1 'string $ strLit str
+      [| string str |]
     TerminalSet rs ->
-      app1 'satisfy $ genRanges rs
+      [| satisfy $(genRanges rs) |]
     NonTerminal nont ->
-      app1c 'Parser $ VarE (mkName $ "udv_" ++ nont)
+      [| Parser $(varE $ mkName $ "udv_" ++ nont) |]
     Empty ->
-      app1 'return $ TupE []
-    
-    Semantic (Sequence es) cf ->
-      DoE $ genBinds 1 es ++ [ NoBindS $ app1 'return $ genCF cf ]
-    Semantic f cf ->
-      DoE [ BindS (VarP $ mkName $ var 1) (genP f)
-          , NoBindS $ app1 'return $ genCF cf ]
+      [| return () |]
 
-    Sequence es ->
-      let binds = genBinds 1 es in
-      let vn = length $ filter isBind binds in
-      DoE $ binds ++
-            [ NoBindS $ app1 'return
-                      $ TupE $ map (VarE . mkName . var) [1..vn] ]
+    Semantic (Sequence es) cf ->
+      doE $ genBinds 1 es ++ [ noBindS [| return $(genCF cf) |] ]
+    Semantic f cf ->
+      genP (Semantic (Sequence [f]) cf)
+
+    Sequence es -> do
+      binds <- sequence $ genBinds 1 es
+      let vn = length $ filter isBind binds
+      doE $ map return binds ++
+               [ noBindS [| return $ $(tupE $ map (varE . mkName . var) [1..vn]) |] ]
 
     Choice es ->
-      foldl1 (app2 '(<|>)) $ map genP es
+      foldl1 (\a b -> [| $a <|> $b |]) $ map genP es
 
     Many f ->
-      app1 'many $ genP f
-
+      [| many $(genP f) |]
     Some f ->
-      app1 'some $ genP f
-
+      [| some $(genP f) |]
     Optional f ->
-      app1 'optional $ genP f
-
+      [| optional $(genP f) |]
     And f ->
-      app1 'expect $ genP f
-
+      [| expect $(genP f) |]
     Not f ->
-      app1 'unexpect $ genP f
+      [| unexpect $(genP f) |]
 
     where
       genBinds _ [] = []
       genBinds ix (f:fs) =
         case f of
           Terminals _ ->
-            NoBindS (genP f) :
+            noBindS (genP f) :
             genBinds ix fs
           And _ ->
-            NoBindS (genP f) :
+            noBindS (genP f) :
             genBinds ix fs
           Not _ ->
-            NoBindS (genP f) :
+            noBindS (genP f) :
             genBinds ix fs
           _ ->
-            BindS (VarP $ mkName $ var ix) (genP f):
+            bindS (varP $ mkName $ var ix) (genP f):
             genBinds (ix+1) fs
 
       isBind (BindS _ _) = True
@@ -144,19 +133,18 @@ generate defs = defDerivs : instDerivs : parse : parsers where
   
   genRanges rs =
     let c = mkName "c" in
-    LamE [VarP c] $ foldl1 (app2 '(||)) $ map (genRange $ VarE c) rs
+    lamE [varP c] $ foldl1 (\a b -> [| $a || $b |]) $ map (genRange c) rs
   genRange c (CharRange l h) =
-    app2 '(&&) (app2 '(<=) (charLit l) c)
-               (app2 '(<=) c (charLit h))
+    [| l <= $(varE c) && $(varE c) <= h |]
   genRange c (CharOne v) =
-    app2 '(==) c (charLit v)
+    [| $(varE c) == v |]
 
   genCF cf =
     case parsed of
       Left _ ->
         error $ "code fragment parse error: " ++ scf
       Right ret ->
-        ret
+        return ret
     where
     parsed = parseExp scf
     scf = concatMap toStr cf
@@ -171,14 +159,6 @@ generate defs = defDerivs : instDerivs : parse : parsers where
   derivType typ = 
     case parseType ("Result UserDerivs (" ++ typ ++ ")") of
       Left err -> error $ "type parse error :" ++ typ ++ ", " ++ err
-      Right t -> t  
+      Right t -> t
 
   var n = "v" ++ show (n :: Int)
-  
-  app1 f x   = AppE (VarE f) x
-  app2 f x y = AppE (AppE (VarE f) x) y
-  app1c f x   = AppE (ConE f) x
-  app2c f x y = AppE (AppE (ConE f) x) y
-
-  strLit = LitE . StringL
-  charLit = LitE . CharL
--- Text/Peggy/Quote.hs
+++ Text/Peggy/Quote.hs
@@ -5,16 +5,20 @@ module Text.Peggy.Quote (
 import Language.Haskell.TH
 import Language.Haskell.TH.Quote
 import Text.Parsec
+import Text.Parsec.Pos
 
 import Text.Peggy.Parser
 import Text.Peggy.CodeGen
 
 peggy :: QuasiQuoter
-peggy = QuasiQuoter { quoteDec = quote }
+peggy = QuasiQuoter { quoteDec = quote, quoteExp = undefined, quotePat = undefined, quoteType = undefined }
 
 quote :: String -> Q [Dec]
-quote txt =
-  case parse syntax "<quasi-quoter>" txt of
+quote txt = do
+  loc <- location
+  case parse (setPosition (initPos loc) >> syntax) (loc_filename loc) txt of
     Left err -> error $ "peggy syntax-error: " ++ show err
-    Right defs ->
-      return $ genCode defs
+    Right defs -> genCode defs
+  where
+    initPos loc = newPos (loc_filename loc) (fst $ loc_start loc) (snd $ loc_start loc)
+    
