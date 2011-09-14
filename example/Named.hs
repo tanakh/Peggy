e409fbc5354f51695add7ecb1712021ead1f4781
Hideyuki Tanaka <tanaka.hideyuki@gmail.com>
2011-09-14 10:49:34 +0900

    add example

---
+++ example/Named.hs
@@ -0,0 +1,19 @@
+{-# Language QuasiQuotes #-}
+
+import Text.Peggy
+import Text.Peggy.Quote
+
+[peggy|
+test :: Int
+  = x:foo "-" y:foo { y-x } 
+
+foo :: Int
+  = s:[0-9]+ { read s }
+
+skip :: () = [ \r\n\t] {()}
+|]
+
+main :: IO ()
+main = do
+  print . runParser test "<stdin>" $ "1-2"
+  print . runParser test "<stdin>" $ "5-3"
