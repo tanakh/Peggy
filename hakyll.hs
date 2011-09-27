21e8bf335e7bdd5054849b91bace5e08c1fb4e95
Hideyuki Tanaka <tanaka.hideyuki@gmail.com>
2011-09-27 13:12:11 -0700

    deploy

--- hakyll.hs
+++ hakyll.hs
@@ -41,7 +41,7 @@ main = hakyllWith config $ do
 
 config :: HakyllConfiguration
 config = defaultHakyllConfiguration { deployCommand = deploy }
-  where deploy = "./hakyll.hs rebuild && cp _site/*.html . && ./hakyll.hs clean && git commit -am 'deploy'"
+  where deploy = "./hakyll.hs rebuild && cp _site/*.html . && cp _site/ja/*.html ./ja && ./hakyll.hs clean && git commit -am 'deploy'"
 
 googlePrettify :: Pandoc -> Pandoc
 googlePrettify doc = bottomUp f doc where
